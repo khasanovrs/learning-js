bd48563c6a00ef7d1222db079139401a1b5fbd65
Khasanov Ruslan <khasanov--ruslan@yandex.ru>
2020-01-28 11:57:00 +0300

    feat: 2.6

--- 1/2.6/examples/2.6.example5.js
+++ 1/2.6/examples/2.6.example5.js
@@ -10,6 +10,6 @@ console.log("4px" - 2); // NaN
 console.log(7 / 0); // Infinity
 console.log("  -9  " + 5); //   -9   5
 console.log("  -9  " - 5); // -14
-console.log(null + 1); // NaN
+console.log(null + 1); // 1
 console.log(undefined + 1); // NaN
-console.log(" \t \n" - 2); // NaN
+console.log(" \t \n" - 2); // -2
